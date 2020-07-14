0c1ff0234cdb64a9eecd618307cd97a15c576b72
Stephanie Fu <fus@mit.edu>
2020-07-14 12:33:37 -0500

    Fix test bug in `optionalOnlyNoGaps()`

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -402,7 +402,7 @@ public final class FindMeetingQueryTest {
     Collection<Event> events = Arrays.asList(
         new Event("Event 1", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0830AM, false), 
             Arrays.asList(PERSON_A)), 
-        new Event("Event 2", TimeRange.fromStartEnd(TIME_0900AM, TimeRange.END_OF_DAY, true), 
+        new Event("Event 2", TimeRange.fromStartEnd(TIME_0830AM, TimeRange.END_OF_DAY, true), 
             Arrays.asList(PERSON_B)));
     
     MeetingRequest request = new MeetingRequest(Arrays.asList(), DURATION_30_MINUTES);
